f39d8171a4d510cdbeeeee5af0f3971ebe5a6d24
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-10-21 14:25:40 -0300

    Inicializando as variáveis ponteiros para as activities

--- app/src/main/java/br/edu/ifsp/dmo1/conversormonetario/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/conversormonetario/MainActivity.kt
@@ -1,11 +1,25 @@
 package br.edu.ifsp.dmo1.conversormonetario
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val DOLLAR_VALUE = 5.50
+
+    private lateinit var inputValue: EditText
+    private lateinit var convertDollarButton: Button
+    private lateinit var conversionResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        inputValue = findViewById(R.id.input_value)
+        convertDollarButton = findViewById(R.id.convert_dollar_button)
+        conversionResult = findViewById(R.id.conversion_result)
     }
 }
