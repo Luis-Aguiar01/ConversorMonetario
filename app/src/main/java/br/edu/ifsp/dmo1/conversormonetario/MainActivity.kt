3ffac0272ce5bda4b87827db4d5ac4989b11a75c
Luis Henrique Aguiar <luishenrique.aguiar@outlook.com>
2024-10-21 14:48:01 -0300

    Primeira implementação da lógica de conversão

--- app/src/main/java/br/edu/ifsp/dmo1/conversormonetario/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/conversormonetario/MainActivity.kt
@@ -10,16 +10,32 @@ class MainActivity : AppCompatActivity() {
 
     private val DOLLAR_VALUE = 5.50
 
-    private lateinit var inputValue: EditText
-    private lateinit var convertDollarButton: Button
+    private lateinit var inputValueEditText: EditText
+    private lateinit var buttonConvertToDollar: Button
+    private lateinit var buttonConvertToReal: Button
     private lateinit var conversionResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        inputValue = findViewById(R.id.input_value)
-        convertDollarButton = findViewById(R.id.convert_dollar_button)
+        inputValueEditText = findViewById(R.id.input_value)
+        buttonConvertToDollar = findViewById(R.id.convert_to_dollar_button)
+        buttonConvertToReal = findViewById(R.id.convert_to_real_button)
         conversionResult = findViewById(R.id.conversion_result)
+
+        var result: Double
+
+        buttonConvertToDollar.setOnClickListener {
+            val inputValue = inputValueEditText.text.toString().toDouble()
+            result = inputValue / DOLLAR_VALUE
+            conversionResult.text = "US $result"
+        }
+
+        buttonConvertToReal.setOnClickListener {
+            val inputValue = inputValueEditText.text.toString().toDouble()
+            result = inputValue * DOLLAR_VALUE
+            conversionResult.text = "R$ $result"
+        }
     }
 }
